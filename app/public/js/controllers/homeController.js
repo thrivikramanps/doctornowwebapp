472000beba491125744b19637f4e9ed2f43a25f8
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-24 20:02:34 -0800

    first commit

--- app/public/js/controllers/homeController.js
+++ app/public/js/controllers/homeController.js
@@ -20,31 +20,6 @@ function HomeController()
 	$('#btn-logout-user').click(function(){ that.attemptLogout(); });
 
 
-
-
-// confirm account deletion //
-	$('#account-form-btn1').click(function(){$('.modal-confirm').modal('show')});
-
-// handle account deletion //
-	$('.modal-confirm .submit').click(function(){ that.deleteAccount(); });
-
-	this.deleteAccount = function()
-	{
-		$('.modal-confirm').modal('hide');
-		var that = this;
-		$.ajax({
-			url: '/delete',
-			type: 'POST',
-			data: { id: $('#userId').val()},
-			success: function(data){
-	 			that.showLockedAlert('Your account has been deleted.<br>Redirecting you back to the homepage.');
-			},
-			error: function(jqXHR){
-				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
-			}
-		});
-	}
-
 	this.attemptLogout = function()
 	{
 		var that = this;
@@ -67,15 +42,7 @@ function HomeController()
 		$('.modal-alert .modal-body p').html(msg);
 		$('.modal-alert').modal('show');
 		$('.modal-alert button').click(function(){window.location.href = '/';})
-		setTimeout(function(){window.location.href = '/';}, 1000);
+		setTimeout(function(){window.location.href = '/';}, 3000);
 	}
 }
 
-HomeController.prototype.onUpdateSuccess = function()
-{
-	$('.modal-alert').modal({ show : false, keyboard : true, backdrop : true });
-	$('.modal-alert .modal-header h3').text('Success!');
-	$('.modal-alert .modal-body p').html('Your account has been updated.');
-	$('.modal-alert').modal('show');
-	$('.modal-alert button').off('click');
-}
