df92b2ff25ee156a53c14b54337b94b0e8b61712
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-28 18:24:39 -0800

    first commit

--- app/public/js/views/booking.js
+++ app/public/js/views/booking.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   var bc = new BookingController();
 
+  $('patient1dob-tf').datepicker();
+  $('patient2dob-tf').datepicker();
+  $('patient3dob-tf').datepicker();
+  $('patient4dob-tf').datepicker();
+  $('rangestartdate-tf').datepicker();
+  $('rangeenddate-tf').datepicker();
+
   
 })
