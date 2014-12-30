1c627084d450f63d2610fbc270020d303d954155
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-29 23:52:13 -0800

    first commit

--- app/public/js/views/booking.js
+++ app/public/js/views/booking.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   var bc = new BookingController();
 
-  $('patient1dob-tf').datepicker();
-  $('patient2dob-tf').datepicker();
-  $('patient3dob-tf').datepicker();
-  $('patient4dob-tf').datepicker();
-  $('rangestartdate-tf').datepicker();
-  $('rangeenddate-tf').datepicker();
+  $('patient1dob-tf').datepicker({changeMonth: true, changeYear: true});
+  $('patient2dob-tf').datepicker({changeMonth: true, changeYear: true});
+  $('patient3dob-tf').datepicker({changeMonth: true, changeYear: true});
+  $('patient4dob-tf').datepicker({changeMonth: true, changeYear: true});
+  $('rangestartdate-tf').datepicker({changeMonth: true, changeYear: true});
+  $('rangeenddate-tf').datepicker({changeMonth: true, changeYear: true});
 
   
-})
+});
--- app/public/js/views/evisitreview.js
+++ app/public/js/views/evisitreview.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var avc = new eVisitReviewController();
+  var evc = new eVisitReviewController();
 
   
 })
