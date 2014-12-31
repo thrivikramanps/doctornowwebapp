6f54470cfb2c99de154e1f6721e674c73f4fb91d
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-31 10:45:22 -0800

    first commit

--- app/server/modules/upload-manager.js
+++ app/server/modules/upload-manager.js
@@ -1,10 +1,28 @@
 var querystring = require("querystring");
 var formidable = require("formidable");
+var path = require('path');
 var fs = require ("fs");
 
 
 exports.upload = function(formData, callback)
 {
 	console.log("entered the upload function");
+
+	var form = new formidable.IncomingForm();
+	form.uploadDir = __dirname + '/app/server/uploads/patientdata';
+	form.keepExtensions = true;
+
+	form.parse(req, function(err, fields, files)){
+		res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received upload:\n\n');
+        console.log("form.bytesReceived");
+        //TESTING
+        console.log("file size: "+JSON.stringify(files.fileUploaded.size));
+        console.log("file path: "+JSON.stringify(files.fileUploaded.path));
+        console.log("file name: "+JSON.stringify(files.fileUploaded.name));
+        console.log("file type: "+JSON.stringify(files.fileUploaded.type));
+        console.log("astModifiedDate: "+JSON.stringify(files.fileUploaded.lastModifiedDate));
+
+	}
 }
 
