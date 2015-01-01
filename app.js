bc5486daf8d02ece4df075306636adbf79cd701f
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-31 18:57:10 -0800

    first commit

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var cookieParser = require('cookie-parser');
 
 var app = express();
 
-
 	app.set('port', 8080);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
@@ -22,8 +21,10 @@ var app = express();
 //	app.use(express.favicon());
 //	app.use(express.logger('dev'));
 	// parse application/json
-    app.use(bodyParser);                        
+    app.use(bodyParser.json());                        
 
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(cookieParser);
 	app.use(session({ resave: true, saveUninitialized: true, 
                       secret: 'thrivi' }));
@@ -31,12 +32,42 @@ var app = express();
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
 
-
 	app.use(errorHandler);
+	var routes = require('./app/server/router')(app);
 
+	app.use('/', routes);
 
-require('./app/server/router')(app);
+		// catch 404 and forward to error handler
+	app.use(function(req, res, next) {
+	    var err = new Error('Not Found');
+	    err.status = 404;
+	    next(err);
+	});
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
-})
+	// error handlers
+
+	// development error handler
+	// will print stacktrace
+	if (app.get('env') === 'development') {
+	    app.use(function(err, req, res, next) {
+	        res.status(err.status || 500);
+	        res.render('error', {
+	            message: err.message,
+	            error: err
+	        });
+	    });
+	}
+
+	// production error handler
+	// no stacktraces leaked to user
+	app.use(function(err, req, res, next) {
+	    res.status(err.status || 500);
+	    res.render('error', {
+	        message: err.message,
+	        error: {}
+	    });
+	});
+
+	http.createServer(app).listen(app.get('port'), function(){
+		console.log("Express server listening on port " + app.get('port'));
+	})
