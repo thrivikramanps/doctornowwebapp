8a6a841c762b1bd2b5aeb0605682d71187338339
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-31 18:35:02 -0800

    first commit

--- app.js
+++ app.js
@@ -3,29 +3,37 @@
 var express = require('express');
 var http = require('http');
 
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var cookieParser = require('cookie-parser');
+
 
 
 var app = express();
 
 
-
-app.configure(function(){
 	app.set('port', 8080);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
 //	app.use(express.favicon());
 //	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9' }));
-	app.use(express.methodOverride());
+	// parse application/json
+    app.use(bodyParser.json());                        
+
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(cookieParser);
+	app.use(session({ secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9' }));
+	app.use(methodOverride);
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
-});
 
 app.configure('development', function(){
-	app.use(express.errorHandler());
+	app.use(errorHandler);
 });
 
 require('./app/server/router')(app);
