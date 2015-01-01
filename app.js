6a2b8615bee4cb3ca663cc656a8e112ad88e5364
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2014-12-31 19:17:33 -0800

    first commit

--- app.js
+++ app.js
@@ -3,71 +3,33 @@
 var express = require('express');
 var http = require('http');
 
-var favicon = require('serve-favicon');
-var methodOverride = require('method-override');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var cookieParser = require('cookie-parser');
-
 
 
 var app = express();
 
+
+
+app.configure(function(){
 	app.set('port', 8080);
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
 //	app.use(express.favicon());
 //	app.use(express.logger('dev'));
-	// parse application/json
-    app.use(bodyParser.json());                        
-
-    // parse application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(cookieParser);
-	app.use(session({ resave: true, saveUninitialized: true, 
-                      secret: 'thrivi' }));
-	app.use(methodOverride);
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: '59B93087-78BC-4EB9-993A-A61FC844F6C9' }));
+	app.use(express.methodOverride());
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
+});
 
-	app.use(errorHandler);
-	var routes = require('./app/server/router')(app);
+app.configure('development', function(){
+	app.use(express.errorHandler());
+});
 
-	app.use('/', routes);
+require('./app/server/router')(app);
 
-		// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-	    var err = new Error('Not Found');
-	    err.status = 404;
-	    next(err);
-	});
-
-	// error handlers
-
-	// development error handler
-	// will print stacktrace
-	if (app.get('env') === 'development') {
-	    app.use(function(err, req, res, next) {
-	        res.status(err.status || 500);
-	        res.render('error', {
-	            message: err.message,
-	            error: err
-	        });
-	    });
-	}
-
-	// production error handler
-	// no stacktraces leaked to user
-	app.use(function(err, req, res, next) {
-	    res.status(err.status || 500);
-	    res.render('error', {
-	        message: err.message,
-	        error: {}
-	    });
-	});
-
-	http.createServer(app).listen(app.get('port'), function(){
-		console.log("Express server listening on port " + app.get('port'));
-	})
+http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+})
