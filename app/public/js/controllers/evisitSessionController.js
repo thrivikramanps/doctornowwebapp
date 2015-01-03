82f8226796fdfaf56e0cd05765ffa4d2bd3ac488
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2015-01-02 22:09:00 -0800

    first commit

--- app/public/js/controllers/evisitSessionController.js
+++ app/public/js/controllers/evisitSessionController.js
@@ -10,9 +10,88 @@ function eVisitSessionController()
 		window.location.href = '/home';		
 	});
 
+	var role = $('#user-role').attr('roletype');
+
+	if (role !== 'Doctor')
+	{
+		$('#notesselector').hide();
+		var general = document.getElementById('generalselector');
+		var history = document.getElementById('historyselector');
+		var notes = document.getElementById('notesselector');
+		general.style.width = "30%";
+		history.style.width = "30%";
+		notes.style.width = "30%";
+	}
+
 
 // handle user logout //
 	$('#btn-logout-user').click(function(){ that.attemptLogout(); });
+
+	$('#generalselector').click(function(){
+		var general = document.getElementById('generalselector');
+		var history = document.getElementById('historyselector');
+		var notes = document.getElementById('notesselector');
+		var list = document.getElementById('listselector');
+		general.style.backgroundColor = "black";
+		general.style.color = "white";
+		history.style.backgroundColor = "white";
+		history.style.color = "black";
+		if (notes){
+			notes.style.backgroundColor = "white";
+			notes.style.color = "black";
+		}
+		if (list && list.display !== "none"){
+			list.style.backgroundColor = "white";
+			list.style.color = "black";
+		}
+
+	});
+
+	$('#historyselector').click(function(){
+		var general = document.getElementById('generalselector');
+		var history = document.getElementById('historyselector');
+		var notes = document.getElementById('notesselector');
+		history.style.backgroundColor = "black";
+		history.style.color = "white";
+		general.style.backgroundColor = "white";
+		general.style.color = "black";
+		if (notes){
+			notes.style.backgroundColor = "white";
+			notes.style.color = "black";
+		}
+		if (list && list.display !== "none"){
+			list.style.backgroundColor = "white";
+			list.style.color = "black";
+		}
+	});
+
+	$('#notesselector').click(function(){
+		var general = document.getElementById('generalselector');
+		var history = document.getElementById('historyselector');
+		var notes = document.getElementById('notesselector');
+		notes.style.backgroundColor = "black";
+		notes.style.color = "white";
+		general.style.backgroundColor = "white";
+		general.style.color = "black";
+		history.style.backgroundColor = "white";
+		history.style.color = "black";
+		if (list && list.display !== "none"){
+			list.style.backgroundColor = "white";
+			list.style.color = "black";
+		}
+	});
+
+	$('#listselector').click(function(){
+		var general = document.getElementById('generalselector');
+		var history = document.getElementById('historyselector');
+		var notes = document.getElementById('notesselector');
+		notes.style.backgroundColor = "white";
+		notes.style.color = "black";
+		general.style.backgroundColor = "white";
+		general.style.color = "black";
+		history.style.backgroundColor = "white";
+		history.style.color = "black";
+	});
 
 
 
