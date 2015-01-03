53ea9d454ffea017d37e7fdd24beb202aab9fe26
Thrivi Subramanian <thrivikraman.ps@gmail.com>
2015-01-02 22:25:36 -0800

    first commit

--- app/server/modules/role-list.js
+++ app/server/modules/role-list.js
@@ -1,7 +1,6 @@
 
 module.exports = [
     {short:"  " , name:"Please select a role"},
-    {short:"Admin" , name:"Admin"},
     {short:"Doctor" , name:"Doctor"},
     {short:"Nursing Home" , name:"Nursing Home"}
 ]
